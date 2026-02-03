06c3fb5461c4c7d48f49d4c72daee039c4065fdd
gghqls0416 <cisshhb@gmail.com>
2026-02-03 16:20:03 +0900

    Update vite.config.ts

--- Hobin Hwang - Porfolio Final/vite.config.ts
+++ Hobin Hwang - Porfolio Final/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,14 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
   },
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   }
 });
